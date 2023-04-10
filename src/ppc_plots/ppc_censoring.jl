35a8c9f0ce092bd1d1439364a9ab55f14674c149
bparbhu <brian.parbhu@gmail.com>
2023-04-10 13:17:27 -0400

    refactor of ppc censoring to account for missing survival module and dfmeta functionality

--- src/ppc_plots/ppc_censoring.jl
+++ src/ppc_plots/ppc_censoring.jl
@@ -1,45 +1,41 @@
-using Gadfly, DataFrames, KernelDensity
+using Gadfly, DataFrames, KernelDensity, Survival, DataFramesMeta, GadflyExtensions
 
-function ppc_km_overlay(y::Vector, yrep::Matrix; status_y = nothing, size = 0.25, alpha = 0.7)
-    n = length(y)
-    n_yrep = size(yrep, 2)
 
-    # Create a DataFrame for plotting
-    df = DataFrame()
+function ppc_km_overlay(y, yrep, status_y, size=0.25, alpha=0.7)
+    @assert isnumeric(status_y)
+    @assert all(status_y .∈ Ref([0, 1]))
 
-    for i in 1:n_yrep
-        ecdfe = ecdf(yrep[:, i])
-        cdf_values = ecdfe.(y)
-        tmp_df = DataFrame(y = y, cdf = cdf_values, yrep = fill("Yrep $i", n))
-        append!(df, tmp_df)
-    end
+    data = ppc_data(y, yrep, group=status_y)
 
-    # Create the overlay plot with Gadfly
-    p = plot(df, x = "y", y = "cdf", color = "yrep", Geom.line, Guide.title("KM Overlay Plot"), Guide.xlabel("Y"), Guide.ylabel("Empirical CDF"), Theme(default_color = colorant"blue", alphas = [alpha]))
+    data = @transform(data,
+        group = ifelse(:is_y, parse.(Float64, string.(:group)), 1))
+
+    sf_form = @formula(Surv(value, group) ~ rep_label)
+    sf = survfit(sf_form, data)
+    fsf = fortify(sf)
+
+    fsf[!, :is_y_color] = categorical(replace.(string.(fsf[!, :strata]), r"\\[rep\\] \\(.*\$" => "rep", "^italic\\(y\\)" => "y"))
+    fsf[!, :is_y_size] = ifelse.(fsf[!, :is_y_color] .== "yrep", size, 1)
+    fsf[!, :is_y_alpha] = ifelse.(fsf[!, :is_y_color] .== "yrep", alpha, 1)
+
+    fsf[!, :strata] = categorical(fsf[!, :strata], levels = reverse(levels(fsf[!, :strata])))
+
+    p = plot(fsf, x=:time, y=:surv, color=:is_y_color, group=:strata, size=:is_y_size, alpha=:is_y_alpha, Geom.step,
+        Guide.xlabel(y_label()), Guide.ylabel(""),
+        Guide.title(""),
+        Theme(line_width=[0.1], line_style=[:dot], line_color=["gray"]),
+        Coord.cartesian(ymin=0, ymax=1, ymin_fixed=true, ymax_fixed=true))
+    return p
+end
+
+
+function ppc_km_overlay_grouped(y, yrep, group, status_y; size=0.25, alpha=0.7)
+    p_overlay = ppc_km_overlay(y, yrep, status_y, size=size, alpha=alpha)
+
+    p = plot(p_overlay, Guide.xlabel(""),
+        Guide.title(""),
+        facet=:group)
 
     return p
 end
 
-function ppc_km_overlay_grouped(y::Vector, yrep::Matrix, group::Vector; status_y = nothing, size = 0.25, alpha = 0.7)
-    n = length(y)
-    n_yrep = size(yrep, 2)
-    unique_groups = unique(group)
-
-    # Create a DataFrame for plotting
-    df = DataFrame()
-
-    for i in 1:n_yrep
-        for grp in unique_groups
-            grp_idx = findall(==(grp), group)
-            ecdfe = ecdf(yrep[grp_idx, i])
-            cdf_values = ecdfe.(y[grp_idx])
-            tmp_df = DataFrame(y = y[grp_idx], cdf = cdf_values, group = grp, yrep = fill("Yrep $i", length(grp_idx)))
-            append!(df, tmp_df)
-        end
-    end
-
-    # Create the grouped overlay plot with Gadfly
-    p = plot(df, x = "y", y = "cdf", color = "yrep", Geom.subplot_grid(Geom.line, free_y_axis = true), group = "group", Guide.title("Grouped KM Overlay Plot"), Guide.xlabel("Y"), Guide.ylabel("Empirical CDF"), Theme(default_color = colorant"blue", alphas = [alpha]))
-
-    return p
-end
