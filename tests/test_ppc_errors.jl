0529ebe3de7b4f1a63fa9f621bd97d71d3ae8069
Brian Parbhu <brian.parbhu@gmail.com>
2023-04-26 02:02:37 -0400

    adding image tests for plots

--- tests/test_ppc_errors.jl
+++ tests/test_ppc_errors.jl
@@ -1,5 +1,6 @@
 using Test
 using Gadfly
+using ReferenceTests
 
 
 @testset "PPC: predictive errors" begin
@@ -23,6 +24,36 @@ using Gadfly
         @test isa(ppc_error_scatter_avg(y, yrep), Gadfly.Plot)
         @test isa(ppc_error_scatter_avg(y, yrep[1:5, :]), Gadfly.Plot)
     end
+
+
+    # Visual tests
+    p_base = ppc_error_hist(vdiff_y, vdiff_yrep[1:3, :])
+    ref_path = "path/to/reference/images/ppc_error_hist.png"
+    test_image(p_base, ref_path)
+
+    p_base = ppc_error_hist_grouped(vdiff_y, vdiff_yrep[1:3, :], vdiff_group)
+    ref_path = "path/to/reference/images/ppc_error_hist_grouped.png"
+    test_image(p_base, ref_path)
+
+    p_base = ppc_error_scatter(vdiff_y, vdiff_yrep[1:3, :])
+    ref_path = "path/to/reference/images/ppc_error_scatter.png"
+    test_image(p_base, ref_path)
+
+    p_base = ppc_error_scatter_avg(vdiff_y, vdiff_yrep)
+    ref_path = "path/to/reference/images/ppc_error_scatter_avg.png"
+    test_image(p_base, ref_path)
+
+    p_base = ppc_error_scatter_avg_grouped(vdiff_y, vdiff_yrep, vdiff_group)
+    ref_path = "path/to/reference/images/ppc_error_scatter_avg_grouped.png"
+    test_image(p_base, ref_path)
+
+    p_base = ppc_error_scatter_avg_vs_x(vdiff_y, vdiff_yrep, x = 1:length(vdiff_y))
+    ref_path = "path/to/reference/images/ppc_error_scatter_avg_vs_x.png"
+    test_image(p_base, ref_path)
+
+    p_base = ppc_error_binned(y, y_rep)
+    ref_path = "path/to/reference/images/ppc_error_binned.png"
+    test_image(p_base, ref_path)
 
 end
 
