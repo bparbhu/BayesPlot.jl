38482317e79f718837dc1097b8c6c2831acf439c
Brian Parbhu <brian.parbhu@gmail.com>
2023-04-26 22:39:29 -0400

    adding additional includsions for module

--- src/BayesPlot.jl
+++ src/BayesPlot.jl
@@ -1,8 +1,25 @@
 module BayesPlot
 
-include("helpers/")
-include("ppc_plots/")
-include("ppd_plots/")
+include("helpers/bayesplot_colors.jl")
+include("helpers/bayesplot_gadfly_themes.jl")
+include("helpers/bayesplot_grid.jl")
+include("helpers/bayesplot_helpers.jl")
+include("helpers/gadfly_helper.jl")
+include("helpers/tidy_params.jl")
+
+include("ppc_plots/ppc_bars.jl")
+include("ppc_plots/ppc_censoring.jl")
+include("ppc_plots/ppc_distributions.jl")
+include("ppc_plots/ppc_errors.jl")
+include("ppc_plots/ppc_helpers.jl")
+include("ppc_plots/ppc_intervals.jl")
+include("ppc_plots/ppc_scatterplots.jl")
+include("ppc_plots/ppc_test_statistics.jl")
+
+
+include("ppd_plots/ppd_distributions.jl")
+
+
 include("mcmc_plots/")
 
 export
